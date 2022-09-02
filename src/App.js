b6e401b87cc33d225a468d385e494fda6b243c37
Jonathan13127 <jonathanmal@hotmail.fr>
2022-09-02 20:10:22 +0200

    Ajout divers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import Wallet from './artifacts/contracts/Wallet.sol/Wallet.json';
 import './App.css';
+import github from './img/icons8-github-90.png';
+import linkedin from './img/linkedin.png'
 
 let WalletAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
@@ -91,7 +93,7 @@ function App() {
         return
       }
     }catch(err){
-      setError('Une erreur est survenue');
+      setError('Une erreur est survenue.');
     }
   }
 
@@ -111,27 +113,59 @@ function App() {
     setAmountWithdraw(e.target.value);
   }
 
+  function pause(ms) 
+  {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function afficherDate() 
+  {
+    while(true) 
+    {
+      await pause(1000);
+      var cejour = new Date();
+      var options = {weekday: "long", year: "numeric", month: "long", day: "2-digit"};
+      var date = cejour.toLocaleDateString("fr-FR", options);
+      var heure = ("0" + cejour.getHours()).slice(-2) + ":" + ("0" + cejour.getMinutes()).slice(-2) + ":" + ("0" + cejour.getSeconds()).slice(-2);
+      var dateheure = date + " " + heure;
+      var dateheure = dateheure.replace(/(^\w{1})|(\s+\w{1})/g, lettre => lettre.toUpperCase());
+      document.getElementById('dateheure').innerHTML = dateheure;
+  }
+  }
+afficherDate();
+
   return (
     <div className="App">
+      <header>
+        <div id="dateheure"></div>
+      </header>
       <div className='container'>
         <div className='logo'>
           <i className="fa-brands fa-ethereum"></i>
         </div>
         <h2>{balance /10**18} <span className='eth'>eth</span></h2>
         <div className='wallet_flex'>
-        <div className='walletG'>
-            <h3>Envoyer de l'ether</h3>
-            <input type="text" placeholder="Montant en Ethers" onChange={changeAmountSend}/>
-            <button onClick={transfer}>Envoyer</button>
-          </div>
+          <div className='walletG'>
+              <h3>Envoyer de l'ether</h3>
+              <input type="text" placeholder="Montant en Ethers" onChange={changeAmountSend}/>
+              <button onClick={transfer}>Envoyer</button>
+            </div>
           <div className='walletD'>
-            <h3>Retirer de l'ether</h3>
-            <input type="text" placeholder="Montant en Ethers" onChange={changeAmountWithdraw}/>
-            <button onClick={withdraw}>Retirer</button>
+              <h3>Retirer de l'ether</h3>
+              <input type="text" placeholder="Montant en Ethers" onChange={changeAmountWithdraw}/>
+              <button onClick={withdraw}>Retirer</button>
           </div>
         </div>
         {error && <p className='error'>{error}</p>}
         {success && <p className='success'>{success}</p>}
+        <footer id="footer">
+          <a href='https://github.com/Jonathan13127' target="_blank">
+            <img id="footer_img" src={github} alt="Mon GitHub"/>
+          </a>
+          <a href='https://www.linkedin.com/in/jonathan-mallia-44b75b1b8/' target="_blank">
+            <img id="footer_img" src={linkedin} alt="Mon Linkedin"/>
+          </a>
+        </footer>
       </div>
     </div>
   );
